b5c1db26631a63ff530de93fa1ff7c13eb82d615
Enbao <ecao25@sjs.org>
2024-07-28 15:51:48 +0900

    four more trials

--- Examples/cg/main2d.m
+++ Examples/cg/main2d.m
@@ -1,42 +1,4 @@
-%-------------------------------------------------------------------------------------------------------------------%
-% 
-% IB2d is an Immersed Boundary Code (IB) for solving fully coupled non-linear 
-% 	fluid-structure interaction models. This version of the code is based off of
-%	Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
-%
-% Author: Nicholas A. Battista
-% Email:  nick.battista@unc.edu
-% Date Created: May 27th, 2015
-% Institution: UNC-CH
-%
-% This code is capable of creating Lagrangian Structures using:
-% 	1. Springs
-% 	2. Beams (*torsional springs)
-% 	3. Target Points
-%	4. Muscle-Model (combined Force-Length-Velocity model, "HIll+(Length-Tension)")
-%   .
-%   .
-%   .
-%
-% One is able to update those Lagrangian Structure parameters, e.g., spring constants, resting lengths, etc
-% 
-%--------------------------------------------------------------------------------------------------------------------%
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% FUNCTION: main2d is the function that gets called to run the code. It
-%           itself, reads in parameters from the input2d file, and passes 
-%           them to the IBM_Driver function to run the simulation
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 function main2d()
-
-% This is the "main" file, which gets called to initialized the Immersed Boundary
-% Simulation. It reads in all the parameters from "input2d", and sends them
-% off to the "IBM_Driver.m" function to actual perform the simulation
 
 warning('off','all');
 addpath('../IBM_Blackbox/','../../IBM_Blackbox/','../../../IBM_Blackbox/','../../../../IBM_Blackbox/');
@@ -50,13 +12,6 @@ assert( exist( 'IBM_Driver.m', 'file' ) == 2, 'IBM_Driver.m not found -> Please 
 fprintf('\n\n');
 fprintf(' |****** IMMERSED BOUNDARY SIMULATION HAS FINISHED! ******|\n\n')
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% Function to read in input files from "input2d" -> renames all
-% quantities appropriately, just as they are in input file
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function [params,struct_name] = give_Me_input2d_Parameters()
 
 filename= 'input2d';  %Name of file to read in
@@ -64,11 +19,7 @@ filename= 'input2d';  %Name of file to read in
 fileID = fopen(filename);
     C1 = textscan(fileID,'%s %s %f','CollectOutput',1,'CommentStyle','%');
     C2 = textscan(fileID,'%s %s %s','CollectOutput',1,'CommentStyle','%');
-
-    
 fclose(fileID);
 
 params = C1{2};
-
 struct_name = C2{1,1};
-
